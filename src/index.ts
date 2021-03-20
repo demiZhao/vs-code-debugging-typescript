b23fa04a1cea79817042f1eacf9cf5ac4acb4a4b
demi zhao <demizhao@demis-MacBook-Pro.local>
2021-03-20 18:58:17 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { logger } from "./logger";
+
+for (let counter = 0; counter < 10; counter++) {
+    logger(counter);
+}
---
+++ src/logger.ts
@@ -0,0 +1,4 @@
+export const logger = (...args: any) => {
+    console.log(...args);
+    console.log(...args);
+}
